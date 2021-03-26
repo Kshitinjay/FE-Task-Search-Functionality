b9385f8f1fc1b60fc4885bb8b1ad9c8ad0b27c74
Kshitinjay <kshitinjay20@gmail.com>
2021-03-26 18:00:57 +0530

    final commit

--- fetasksearchfunction/src/App.js
+++ fetasksearchfunction/src/App.js
@@ -4,35 +4,36 @@ import Result from "./components/Result/";
 import "./App.css";
 
 function App() {
-  const [item, setItem] = useState("nature");
+  const [item, setItem] = useState("");
   const [data, setData] = useState([]);
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(5);
 
   const handleInc = () => {
-    if (counter < 100) {
-      setCounter(counter + 10);
+    if (counter < 50) {
+      setCounter(counter + 5);
     }
   };
 
   const handleDec = () => {
-    if (counter > 10) {
-      setCounter(counter - 10);
+    if (counter > 5) {
+      setCounter(counter - 5);
     }
   };
 
   const handleChange = (event) => {
     setItem(event.target.value);
-    console.log(event.target.value);
   };
   useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=20869091-ffe7c1dfbbcd0b2fb444f1fb6&q=${item}&per_page=${counter}`
-    )
-      .then((res) => res.json())
-      .then((res) => setData(res.hits));
+    if (item.length >= 3) {
+      fetch(
+        `https://pixabay.com/api/?key=20869091-ffe7c1dfbbcd0b2fb444f1fb6&q=${item}&per_page=${counter}`
+      )
+        .then((res) => res.json())
+        .then((res) => setData(res.hits));
+    }
   }, [item, counter]);
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <div className="App">
